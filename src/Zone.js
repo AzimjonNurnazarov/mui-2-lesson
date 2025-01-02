e9f0cbe036cf326501e77cf811ac47e53f724fc3
Azimjon <azimn1993@gmail.com>
2025-01-03 01:08:50 +0500

    mui

--- src/Zone.js
+++ src/Zone.js
@@ -1,24 +1,21 @@
 import { Box, Button, Grid, ImageList, Paper, Typography } from "@mui/material";
 import React from "react";
-import { styled } from "@mui/material/styles";
+/* import { styled } from "@mui/material/styles";
 import CircleIcon from "@mui/icons-material/Circle";
-import SettingsIcon from "@mui/icons-material/Settings";
+import SettingsIcon from "@mui/icons-material/Settings"; */
 
-const Item = styled(Paper)(({ theme }) => ({
+/* const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.error,
-}));
+})); */
 
-// const ImgBox = styled.img`
-//   border: 1px solid red;
-//   width: 100%;
-// `;
+
 
 export default function Zone() {
-  return (
+/*   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container>
         <Grid item xs={6}>
@@ -147,5 +144,5 @@ export default function Zone() {
         </Grid>
       </Grid>
     </Box>
-  );
+  ); */
 }
